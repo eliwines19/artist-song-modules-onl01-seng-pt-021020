f9560a470bc25e51555152487a898d5b645b958e
Elijah wines <elijahwines84@yahoo.com>
2020-03-11 00:45:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,34 +9,32 @@ class Artist
   @@artists = []
 
   def initialize
-    # @@artists << self
-   # self.class.all << self will be replaced by super
-   super
-   @songs = []
+    @@artists << self
+    @songs = []
   end
 
-  # def self.find_by_name(name)
-    # @@artists.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
   end
 
-  # def self.reset_all
-    # self.all.clear
-  # end
+  def self.reset_all
+    self.all.clear
+  end
 
-  # def self.count
-    # self.all.count
-  # end
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-    # name.downcase.gsub(' ', '-')
-  # end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
